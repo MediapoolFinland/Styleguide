03da54d9289bdebbd03f16a399017e793286366f
mcela <nicolas.makela@gmail.com>
2016-05-27 15:20:07 +0300

    Added a note

--- layouts/index.php
+++ layouts/index.php
@@ -65,6 +65,7 @@ function render($file, $dataArr = array()) {
 		
 	<h2>Row with four images as columns</h2>
 	<p>Simple 4 column block with images that have a hover active vertically aligned body content.</p>
+	<p><i>Note: the vertical align css classes are in the <b>_library.scss</b> -file.</i></p>
 		<?php
 		render("row--four-columns-with-imgs.twig", array("image" => "https://placehold.it/227x339", "body" => "<strong>Lorem ipsum doret</strong><br> ipsum hipsum hipsuli pipsuli"));
 		?>
