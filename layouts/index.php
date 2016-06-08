e0569a9076350a2d72d1b807a12694dce42bf2c6
mcela <nicolas.makela@gmail.com>
2016-06-08 16:21:19 +0300

    Added variations for imgs

--- layouts/index.php
+++ layouts/index.php
@@ -111,13 +111,13 @@ render("element--hilighted-articles.twig", array("articles" => array(
 		<p>Image that has a hover active vertically aligned body content.</p>
 		<p><i>Note: the vertical align css classes are in the <b>_library.scss</b> -file.</i></p>
 		<?php
-render("element--img--hover.twig", array("image" => "https://placehold.it/227x339", "body" => "<strong>Lorem ipsum doret</strong><br> ipsum hipsum hipsuli pipsuli"));
+render("element--img--hover.twig", array("image" => "https://placehold.it/200x300", "body" => "<strong>Lorem ipsum doret</strong><br> ipsum hipsum hipsuli pipsuli"));
 ?>
 
 	<h2>Image with content blocks</h2>
 		<p></p>
 		<?php
-render("element--img--block.twig", array("image" => "https://placehold.it/227x339", "title" => "Trenssi", "desc" => "Chanel"));
+render("element--img--block.twig", array("image" => "https://placehold.it/200x300", "title" => "Trenssi", "desc" => "Chanel"));
 ?>
 </div>
 <script type="text/javascript" src="../assets/js/app.js"></script>
